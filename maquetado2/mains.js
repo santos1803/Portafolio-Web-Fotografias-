baec15f11344e157fd8753f1a407204ab7673728
santos1803 <117311895+santos1803@users.noreply.github.com>
2023-05-29 20:48:01 -0400

    Add files via upload

---
+++ maquetado2/mains.js
@@ -0,0 +1,9 @@
+//Usada para el index
+
+function mostrar() {
+    document.getElementById("cvs").style.display = "block";
+}
+
+function cerrar() {
+    document.getElementById("cvs").style.display = "none"
+}
